a3f484c116f4de216448f94f470590bb21ee3a7f
AbdullahAamir7 <abdullah2aamir@gmail.com>
2024-09-28 23:15:18 +0500

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,17 @@
-my_dict={'Name':"Ali",'Age':17,'Grade':"A+"}
+dict1={162:"Zain",335:"Saad",158:"Umer"}
+dict2={'fruits':["Apple","Mango","Bnana"]}
 
+print(dict1[335])
+print(dict2)
 
-my_dict['Age']=18
+dict1[354]="ali"
+print(dict1)
 
-my_dict['Gender']="Male"
+dict1[158]="Hassan"
+print(dict1)
 
-print(my_dict)
+dict1.pop(162)
+print(dict1)
+
+dict1.clear()
+print(dict1)
